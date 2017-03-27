954fb04ef645af21aeb50eb8672136cacedeb3a9
kyonru <the.robert.007@gmail.com>
2017-03-26 21:59:21 -0400

    Eliminando archivos por defecto de google. El menu está funcional

--- TextLater/app/src/main/java/com/gestion/textlater/textlater/MainActivity.java
+++ TextLater/app/src/main/java/com/gestion/textlater/textlater/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,6 +27,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        MenuFragmentAdapter adapter = new MenuFragmentAdapter(getSupportFragmentManager(), this);
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
 
         fabEnviar = (FloatingActionButton) findViewById(R.id.fab_send);
         fabGmail = (FloatingActionButton) findViewById(R.id.fab_gmail);

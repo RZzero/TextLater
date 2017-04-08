eef121913ba475fbbba3167499e940c997fffb23
kyonru <the.robert.007@gmail.com>
2017-04-08 06:50:08 -0400

    fixing a bug

--- TextLater/app/src/main/java/com/gestion/textlater/textlater/MenuActivity.java
+++ TextLater/app/src/main/java/com/gestion/textlater/textlater/MenuActivity.java
@@ -85,6 +85,8 @@ public class MenuActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_configuracion) {
+            Intent i = new Intent(MenuActivity.this, PrincipalActivity.class);
+            this.startActivity(i);
 
         } else if (id == R.id.nav_nosotros) {
 
